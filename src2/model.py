1f489a47f16943a00b0ea73b26c4da88ca3ab239
tianqi <12720211+tianqiisgood@user.noreply.gitee.com>
2025-04-28 12:00:18 +0800

    4/28/7

--- src2/model.py
+++ src2/model.py
@@ -3,10 +3,10 @@ from tensorflow.keras import layers, Model
 from params import config
 
 
+# 空间注意力
 class TemporalAttention(layers.Layer):
-    def __init__(self, units, **kwargs):
-        super(TemporalAttention, self).__init__(**kwargs)
-        self.units = units
+    def __init__(self, units):
+        super(TemporalAttention, self).__init__()
         self.W = layers.Dense(units)
         self.V = layers.Dense(1)
 
@@ -16,115 +16,98 @@ class TemporalAttention(layers.Layer):
         context_vector = inputs * attention_weights
         return context_vector
 
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "units": self.units
-        })
-        return config
-
-
+# 时间注意力
 class SpatialAttention(layers.Layer):
-    def __init__(self, kernel_size=7, **kwargs):
-        super(SpatialAttention, self).__init__(**kwargs)
-        self.kernel_size = kernel_size
-        self.conv = layers.Conv1D(1, kernel_size=kernel_size, padding='same')
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv1d = layers.Conv1D(1, kernel_size=kernel_size, padding='same')
 
     def call(self, inputs):
-        avg_pool = tf.reduce_mean(inputs, axis=-1, keepdims=True)
-        max_pool = tf.reduce_max(inputs, axis=-1, keepdims=True)
-        concat = tf.concat([avg_pool, max_pool], axis=-1)
-        spatial_map = self.conv(concat)
-        spatial_map = tf.nn.sigmoid(spatial_map)
-        return inputs * spatial_map
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "kernel_size": self.kernel_size
-        })
-        return config
+        avg_pool = tf.reduce_mean(inputs, axis=2, keepdims=True)
+        max_pool = tf.reduce_max(inputs, axis=2, keepdims=True)
+        concat = tf.concat([avg_pool, max_pool], axis=2)
+        spatial_attention = tf.nn.sigmoid(self.conv1d(concat))
+        return inputs * spatial_attention
 
 
 class BiLSTM_CNN(Model):
-    def __init__(self, config_dict=None, **kwargs):
-        super(BiLSTM_CNN, self).__init__(**kwargs)
+    def __init__(self, model_config=None):
+        super(BiLSTM_CNN, self).__init__()
 
-        # 使用默认配置或传入的配置
-        self.model_config = {
-            'cnn_filters': 64,
-            'cnn_kernel_sizes': [5, 7, 9],
-            'bilstm_units': [128, 64],
+        self.config = {
+            'cnn_filters': 64,  # 'cnn_filters': n/2
+            'cnn_kernel_sizes': [3, 5, 7],
+            'bilstm_units': [128, 64],  # [第一层units, 第二层units]
             'dropout_rate': 0.2,
             'recurrent_dropout': 0.2,
-            'spatial_attention_kernel': 9,
-            'dense_units': [256, 128],
+            'spatial_attention_kernel': 7,  # 与最大匹配
+            'dense_units': [256, 128],  # 'dense_units': [n*2, n]
             'l2_reg': 0.001,
             'classifier_dropout': 0.3,
-            'num_classes': config.NUM_CLASSES
+            'num_classes': config.NUM_CLASSES  # 2分类
         }
-        if config_dict:
-            self.model_config.update(config_dict)
+        if model_config:
+            self.config.update(model_config)
 
         # 多尺度CNN特征提取
         self.multi_scale_convs = []
-        for kernel_size in self.model_config['cnn_kernel_sizes']:
+        for kernel_size in self.config['cnn_kernel_sizes']:
             conv_block = tf.keras.Sequential([
                 layers.Conv1D(
-                    self.model_config['cnn_filters'],
+                    self.config['cnn_filters'],
                     kernel_size=kernel_size,
                     padding='same',
                     activation='relu'
                 ),
                 layers.BatchNormalization(),
                 layers.MaxPooling1D(2, padding='same'),
-                layers.Dropout(self.model_config['dropout_rate'])
+                layers.Dropout(self.config['dropout_rate'])
             ])
             self.multi_scale_convs.append(conv_block)
 
         # 特征融合层
         self.feature_fusion = layers.Concatenate()
         self.fusion_conv = layers.Conv1D(
-            self.model_config['cnn_filters'] * 2,
+            self.config['cnn_filters'] * 2,
             kernel_size=1,
             padding='same'
         )
 
-        # 双向LSTM层
+        # 层次化BiLSTM
         self.bilstm_layers = []
-        for units in self.model_config['bilstm_units']:
+        for units in self.config['bilstm_units']:
             self.bilstm_layers.extend([
                 layers.Bidirectional(
                     layers.LSTM(
                         units,
                         return_sequences=True,
-                        recurrent_dropout=self.model_config['recurrent_dropout']
+                        recurrent_dropout=self.config['recurrent_dropout']
                     )
                 ),
                 layers.BatchNormalization()
             ])
 
         # 注意力机制
-        self.temporal_attention = TemporalAttention(self.model_config['bilstm_units'][0] * 2)
+        self.temporal_attention = TemporalAttention(self.config['bilstm_units'][0] * 2)
         self.spatial_attention = SpatialAttention(
-            kernel_size=self.model_config['spatial_attention_kernel']
+            kernel_size=self.config['spatial_attention_kernel']
         )
 
         # 分类头
-        self.classifier_layers = []
-        for units in self.model_config['dense_units']:
-            self.classifier_layers.extend([
-                layers.Dense(
-                    units,
-                    activation='relu',
-                    kernel_regularizer=tf.keras.regularizers.l2(self.model_config['l2_reg'])
-                ),
-                layers.BatchNormalization(),
-                layers.Dropout(self.model_config['classifier_dropout'])
-            ])
-        self.classifier_layers.append(
-            layers.Dense(self.model_config['num_classes'], activation='softmax')
-        )
+        self.classifier = self._build_classifier()
+
+    def _build_classifier(self):
+        classifier = tf.keras.Sequential()
+        for units in self.config['dense_units']:
+            classifier.add(layers.Dense(
+                units,
+                activation='relu',
+                kernel_regularizer=tf.keras.regularizers.l2(self.config['l2_reg'])
+            ))
+            classifier.add(layers.BatchNormalization())
+            classifier.add(layers.Dropout(self.config['classifier_dropout']))
+        classifier.add(layers.Dense(self.config['num_classes'], activation='softmax'))
+        return classifier
 
     def call(self, inputs):
         # 多尺度CNN特征提取
@@ -136,35 +119,24 @@ class BiLSTM_CNN(Model):
         fused_features = self.feature_fusion(conv_features)
         fused_features = self.fusion_conv(fused_features)
 
+        # 空间注意力
+        spatial_context = self.spatial_attention(fused_features)
+
         # BiLSTM处理
-        lstm_features = fused_features
+        lstm_features = spatial_context
         for layer in self.bilstm_layers:
             lstm_features = layer(lstm_features)
 
-        # 双重注意力机制
+        # 时间注意力
         temporal_context = self.temporal_attention(lstm_features)
-        spatial_context = self.spatial_attention(temporal_context)
 
         # 全局池化
-        global_features = tf.reduce_mean(spatial_context, axis=1)
+        global_features = tf.reduce_mean(temporal_context, axis=1)
 
         # 分类
-        x = global_features
-        for layer in self.classifier_layers:
-            x = layer(x)
-        return x
+        return self.classifier(global_features)
 
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "config_dict": self.model_config
-        })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(config_dict=config.get("config_dict", None))
-
+    # 损失函数
     def build_custom_loss(self):
         def categorical_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
             epsilon = 1e-7
